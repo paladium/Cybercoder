a57760402036bc9962a1dfe42e938fcd4761057b
Dragma <musaevilkin29@gmail.com>
2020-04-12 12:43:16 +0400

    List of articles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,31 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    <p v-for="post in $page.posts.edges" :key="post.node.path">
+        <g-link :to="post.node.path">
+            {{post.node.title}}
+        </g-link>
     </p>
 
   </Layout>
 </template>
 
+<page-query>
+query Posts{
+    posts: allPost{
+        edges{
+            node{
+                title
+                path
+            }
+        }
+    }
+}
+</page-query>
+
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Welcome to Future'
   }
 }
 </script>
