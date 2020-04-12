217a1f53f4a9f0cf62415729dd29ab1ebb8864f7
Dragma <musaevilkin29@gmail.com>
2020-04-12 16:53:23 +0400

    Better logo

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,22 +4,24 @@
             class="flex bg-white border-b border-gray-200 fixed top-0 inset-x-0 z-100 h-16 items-center"
         >
             <div class="w-full max-w-screen-xl relative mx-auto px-6">
-                <div class="flex items-center -mx-6">
+                <div class="flex items-center justify-center mx-6">
                     <div class="lg:w-1/4 xl:w-1/5 pl-6 pr-6">
                         <div class="flex items-center">
-                            <g-link to="/" class="block font-bold">Cybercoder</g-link>
+                            <g-link to="/" class="block font-bold">
+                                <g-image src="~/logo.png"></g-image>
+                            </g-link>
                         </div>
                     </div>
 
-                    <div class="flex flex-grow lg:w-3/4 xl:w-4/5">
+                    <!-- <div class="flex flex-grow lg:w-3/4 xl:w-4/5">
                         <div class="flex items-center justify-between w-1/4 px-6">
                             <div class="flex justify-end items-center">
-                                <!-- <g-link>
+                                <g-link>
                                     About
-                                </g-link> -->
+                                </g-link>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
